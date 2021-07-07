cd794c38900e8fbab7f567ecf3e2e001b7747a3d
fatih.dogmus <fdogmus06@gmail.com>
2021-07-07 16:30:08 +0300

    Initial commit

---
+++ src/main/java/yte/intern/springdata/SpringDataApplication.java
@@ -0,0 +1,12 @@
+package yte.intern.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+}
---
+++ src/test/java/yte/intern/springdata/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package yte.intern.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
