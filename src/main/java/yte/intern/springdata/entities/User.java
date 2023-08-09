3cf1bdede30a7d043fa73e22e3733866ec796581
fatih.dogmus <fatih.dogmus@tubitak.gov.tr>
2023-08-09 16:28:17 +0300

    javax -> jakarta migration

--- src/main/java/yte/intern/springdata/entities/User.java
+++ src/main/java/yte/intern/springdata/entities/User.java
@@ -5,10 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @Getter
