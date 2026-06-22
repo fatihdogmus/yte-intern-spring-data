5e589acf8a43e8ac491cabc24dbe040ed178e52e
fatihdogmus <fdogmus06@gmail.com>
2026-06-22 11:28:27 +0300

    spring boot versiyonu yükseltildi

--- src/main/java/yte/intern/springdata/SpringDataApplication.java
+++ src/main/java/yte/intern/springdata/SpringDataApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataApplication {
 
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 }
